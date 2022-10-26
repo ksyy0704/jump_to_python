9a61c8969a9b5732fb09b87e57b088e35ae94703
ksyy0704@gmail.com <ksyy0704@gmail.com>
2022-10-26 20:31:52 +0900

    02-1.숫자형

---
+++ 02-1.py
@@ -0,0 +1,31 @@
+#숫자형
+#정수형
+a = 123
+a = -178
+a = 0
+
+#실수형
+b = 1.2
+b = -3.45
+b = 4.24E10
+b = 4.24e-10  #e와 E 둘 중 어느 것을 사용해도 무방.
+
+
+#8진수와 16진수
+a = 0o177
+b = 0xABC
+
+#사칙연산
+a = 3
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b) #a의 b제곱
+print(a%b)
+print(a//b) #나눗셈 후 몫 반환
+
+#나혼자코딩!
+print(14//3)
+print(14%3)
